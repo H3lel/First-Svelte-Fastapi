56e33c01ce57852c511091e5208b46641f288bd5
H3lel <moghadamartin@gmail.com>
2025-12-02 02:01:09 +0330

    v2

--- code/frontend/src/routes/+page.svelte
+++ code/frontend/src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    
+    var tasks:any = [
+        {"id":1,"name":"labor1","description":"test1"},
+        {"id":2,"name":"labor2","description":"test2"},
+    ];
+</script>
+
+
+<h1>TODO APP</h1>
+{#if tasks}
+    <table>
+        <thead>
+            <tr>
+                <th>Task Id</th>
+                <th>Task name</th>
+                <th>Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each tasks as task}
+                <tr>
+                    <td>{task.id}</td>
+                    <td>{task.name}</td>
+                    <td>{task.description}</td>
+
+
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+{:else}
+    <h1>There is no task</h1>
+{/if}
